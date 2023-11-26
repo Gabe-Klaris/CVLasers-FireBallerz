a2599455dd07e60ca87bbd5f7449da9dd8d1c7d3
Henry-Cat <57220048+Henry-Cat@users.noreply.github.com>
2023-11-26 14:43:37 -0500

    Adding robomodules directly because im dumb as hell

--- webcam_test_local.py
+++ webcam_test_local.py
@@ -1,38 +1,42 @@
-# NAME: read_local_camera_feed.py
-# PURPOSE: reading and displaying (using OpenCV) a video feed from a local 
-#          camera on a Raspberry Pi
-# AUTHOR: Mostly this guy tbh https://pyimagesearch.com/2015/03/30/accessing-the-raspberry-pi-camera-with-opencv-and-python/
+#!/usr/bin/env python3
+
+# NAME: local_camera_reader.py
+# PURPOSE: wrapper class for reading the most recent frame from a camera stream 
+#          on the pi
+# AUTHOR: Emma Bethel
 
 import cv2
-
-from picamera import PiCamera
-from picamera.array import PiRGBArray
+from picamera2 import Picamera2
 from time import sleep
 
-# setting resulution (can definitely play this; these cameras should be capable 
-# of up to 1080p, though larger image sizes take longer to process)
-RESOLUTION = (640, 480)
 
-# set up/connect to camera
-camera = PiCamera()
-camera.resolution = RESOLUTION
-camera.framerate = 32
-raw_capture = PiRGBArray(camera, size=RESOLUTION)
+class LocalCameraFeed:
 
-# allow the camera to warmup
-sleep(0.1)
+    # PURPOSE: constructor
+    # PARAMTERS: N/A
+    # RETURNS: N/A
+    def __init__(self):
+        self.cam = Picamera2()
+        self.cam.start()
+        sleep(1)
 
-# capture frames from the camera
-for frame in camera.capture_continuous(raw_capture, format="bgr", use_video_port=True):
-	# grab the raw NumPy array representing the current frame
-	image = frame.array
+    # PURPOSE: retrieves and parses the most recent frame in the camera stream
+    # PARAMETERS: N/A
+    # RETURNS: the frame, as a 3D numpy array (such that each pixel is a trio 
+    #          of RGB values)
+    def read(self):
+        frame = self.cam.capture_array('main')
+        
+        return cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
 
-	# show the frame
-	cv2.imshow("Frame", image)
-	
-    # clear the stream in preparation for the next frame
-	raw_capture.truncate(0)
 
-	# if the `q` key was pressed, break from the loop
-	if cv2.waitKey(1) & 0xFF == ord("q"):
-		break       
+if __name__ == '__main__':
+    feed = LocalCameraFeed()
+
+    while True:
+        frame = feed.read()
+
+        cv2.imshow('Camera Feed', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
