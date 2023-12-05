1074ce9137e440ebe7a36f5587991963a17dfc53
Henry-Cat <57220048+Henry-Cat@users.noreply.github.com>
2023-12-04 21:48:32 -0500

    Goofy idea. will it work? nobody knows

--- vision_module.py
+++ vision_module.py
@@ -7,6 +7,7 @@ import robomodules as rm
 from messages import MsgType, message_buffers, TiltCommand, RotationCommand
 from local_camera_reader import LocalCameraFeed
 from shape_finders import find_triangles, find_circles, find_octagons, find_squares
+import time
 
 # retrieving address and port of robomodules server (from env vars)
 ADDRESS = os.environ.get("LOCAL_ADDRESS","localhost")
@@ -29,7 +30,7 @@ class VisionModule(rm.ProtoModule):
 
         # Scanning state variables
         self.scan_direction = 1 # 1 for up, -1 for down
-        self.camera_feed = LocalCameraFeed()
+        # self.camera_feed = LocalCameraFeed()
 
     # runs every time one of the subscribed-to message types is received
     def msg_received(self, msg, msg_type):
@@ -43,11 +44,31 @@ class VisionModule(rm.ProtoModule):
         if False: # For moving camera
             self.tilt(float(input("Tilt: ")))
             self.rotate(float(input("Rotate: ")))
+            return
         # self.turn_to_target((500, 300))
         
         frame = self.camera_feed.read()
-        centers = find_triangles(frame, "red")
-        print(centers)
+        target_x, target_y = find_triangles(frame, "red")[0]
+        print(target_x)
+        self.rotate_to_target(target_x)
+
+    def find_target(self, shape_function, color):
+        results = dict()
+        for i in range(10):
+            frame = self.camera_feed.read()
+            centers = shape_function(frame, color)
+            for center in centers:
+                # Add center to results if close enough to existing result
+                for result in results:
+                    if abs(result[0] - center[0]) < 10 and abs(result[1] - center[1]) < 10:
+                        results[result] += 1
+                        break
+                    else:
+                        results[center] = 1
+            time.sleep(0.05)
+
+        max_center = max(results, key=results.get)
+        return max_center if results[max_center] >= 3 else None
 
     def scan_tick(self):
         # Get tilt direction
@@ -61,16 +82,16 @@ class VisionModule(rm.ProtoModule):
         self.tilt(tilt)
         self.rotate(SCAN_ROTATION_STEP)
     
-    def turn_to_target(self, target):
+    def rotate_to_target(self, target_x):
         # Target is an x, y tuple representing pixel in the camera we want to turn to
         # If far enough away, turn to target
-        if (target[0] - LASER_TARGET[0]) ** 2 + (target[1] - LASER_TARGET[1]) ** 2 > 1000:
-            print("Turning by " + str((target[0] - LASER_TARGET[0]) / 10))
-            self.rotate((target[0] - LASER_TARGET[0]) / 10)
+        if (target_x - LASER_TARGET[0]) > 30:
+            print("Turning by " + str((target_x - LASER_TARGET[0]) / 10))
+            self.rotate((target_x - LASER_TARGET[0]) / 10)
             return False
         
         # If closer but not close enough, turn a fixed amount
-        elif (target[0] - LASER_TARGET[0]) ** 2 + (target[1] - LASER_TARGET[1]) ** 2 > 100:
+        elif (target_x - LASER_TARGET[0]) > 5:
             print("Turning by 5")
             self.rotate(5)
             return False
